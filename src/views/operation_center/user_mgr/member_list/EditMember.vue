023eefbab9b655df00f2c34aa1d284ce519e3bc6
qiuchentao <qiuchentao@zmfilm.com>
2019-08-02 11:21:30 +0800

    删除按钮

--- src/views/operation_center/user_mgr/member_list/EditMember.vue
+++ src/views/operation_center/user_mgr/member_list/EditMember.vue
@@ -1,33 +1,49 @@
 <template>
   <div class="app-container">
-    <el-form label-width="120px"  style="width:600px;" :model="params" :rules="rules" ref="form">
-      <el-form-item label="手机号码" prop="userName" >
-          <el-input v-model="params.mobile" placeholder="请输入手机号码" disabled></el-input>
+    <el-form label-width="120px" style="width:600px;" :model="params" :rules="rules" ref="form">
+      <el-form-item label="手机号码" prop="userName">
+        <el-input clearable v-model="params.mobile" placeholder="请输入手机号码" disabled></el-input>
       </el-form-item>
       <el-form-item label="性别" prop="realName">
         <!-- <el-radio v-model="params.sex" label="" >保密</el-radio> -->
-        <el-radio v-model="params.sex" label="1" >男</el-radio>
-        <el-radio v-model="params.sex" label="2" >女</el-radio>
+        <el-radio v-model="params.sex" label="1">男</el-radio>
+        <el-radio v-model="params.sex" label="2">女</el-radio>
       </el-form-item>
       <el-form-item label="生日" prop="email">
-        <el-date-picker v-model="params.birthday" type="date" value-format="yyyy-MM-dd"  placeholder="选择日期">
-    </el-date-picker>
+        <el-date-picker
+          v-model="params.birthday"
+          type="date"
+          value-format="yyyy-MM-dd"
+          placeholder="选择日期"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item label="城市" prop="roleId">
         <city-cascader v-model="area" :rang="1" :clearable="true" style="width:220px"></city-cascader>
       </el-form-item>
       <el-form-item label="新密码" prop="passWord">
-        <el-input v-model.trim="params.passWord" type="password" placeholder="请输入登录密码" disabled></el-input>
+        <el-input
+          clearable
+          v-model.trim="params.passWord"
+          type="password"
+          placeholder="请输入登录密码"
+          disabled
+        ></el-input>
       </el-form-item>
-      <el-form-item label="确认密码" prop="rePassWord" >
-        <el-input v-model.trim="params.rePassWord"  type="password" placeholder="请输入确认密码" disabled></el-input>
+      <el-form-item label="确认密码" prop="rePassWord">
+        <el-input
+          clearable
+          v-model.trim="params.rePassWord"
+          type="password"
+          placeholder="请输入确认密码"
+          disabled
+        ></el-input>
       </el-form-item>
       <el-form-item label="账户启用状态" prop="remark">
         <el-switch v-model="params.status" active-value="1" inactive-value="2"></el-switch>
       </el-form-item>
       <el-form-item style="text-align:center">
         <el-button type="primary" @click="save">保存</el-button>
-        <el-button @click="closeTab" >取消</el-button>
+        <el-button @click="closeTab">取消</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -41,17 +57,14 @@ export default {
   components: { CityCascader },
   data() {
     return {
-      params: {
-      },
+      params: {},
       area: [],
-      rules: {
-
-      }
+      rules: {}
     }
   },
   methods: {
     save() {
-      this.$refs.form.validate(async(valid) => {
+      this.$refs.form.validate(async valid => {
         if (valid) {
           const params = Object.assign({}, this.params)
           if (this.area) {
@@ -71,14 +84,16 @@ export default {
     // 编辑
     if (this.$route.query.id) {
       this.params = await getMemberInfo(this.$route.query.id)
-      this.area = [this.params.provinceId, this.params.cityId].filter(item => item)
+      this.area = [this.params.provinceId, this.params.cityId].filter(
+        item => item
+      )
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.sub-menu{
+.sub-menu {
   margin: 10px;
 }
 </style>
--- src/views/price_center/price_mgr/channel_plan/index.vue
+++ src/views/price_center/price_mgr/channel_plan/index.vue
@@ -1,37 +1,71 @@
 <template>
-    <div class="app-container">
-         <el-form inline label-width="120px">
-            <el-form-item label="影院名称">
-              <el-input v-model="query.cinemaName" style="width:200px;" placeholder="请输入影院名称或编码模糊查询"></el-input>
-              <!-- <cinema-id-selector v-model="query.cinemaId"  style="width:200px;"></cinema-id-selector> -->
-            </el-form-item>
-            <el-form-item label="商家名称">
-              <channel-id-selector v-model="query.channelId"  style="width:200px;"></channel-id-selector>
-            </el-form-item>
-            <el-form-item label="省/市">
-             <city-cascader ref="city" v-model="query.area" :rang="1" :clearable="true" placeholder="请选择" style="width:170px;"></city-cascader>
-            </el-form-item>
-            <el-form-item >
-              <el-button type="primary" icon="el-icon-search"  @click="refreshTable">查询</el-button>
-              <el-button type="warning" icon="el-icon-refresh"  @click="updatePlan(query.channelId,query.cinemaId,query.cinemaName)">更新排期及价格</el-button>
-            </el-form-item>
-         </el-form>
-         <page-table ref="table" index :query="query" :fetch="queryTable">
-            <el-table-column min-width="180"  label="影院名称" align="center" prop="cinemaName" show-overflow-tooltip></el-table-column>
-            <el-table-column min-width="180"  label="影院定价分组" align="center" prop="priceRightName" ></el-table-column>
-            <el-table-column min-width="140"  label="商家名称" align="center" prop="channelName" show-overflow-tooltip></el-table-column>
-            <el-table-column width="80"  label="总排期数" align="center" prop="totoalNum"></el-table-column>
-            <el-table-column width="100"  label="可售排期数" align="center" prop="sellNum"></el-table-column>
-            <el-table-column min-width="100"  label="不可售排期数" align="center" prop="unSellNum"></el-table-column>
-            <el-table-column width="140"  label="更新排期时间" align="center" prop="updateTime" show-overflow-tooltip></el-table-column>
-            <el-table-column min-width="200"  label="操作" align="center" prop="oper" fixed="right">
-              <template slot-scope="{row}">
-                <el-button type="text" @click="goToManage(row)">管理排期</el-button>
-                <el-button type="text" @click="updatePlan(row.channelId,row.cinemaId,row.cinemaName)">更新排期</el-button>
-              </template>
-            </el-table-column>
-         </page-table>
-    </div>
+  <div class="app-container">
+    <el-form inline label-width="120px">
+      <el-form-item label="影院名称">
+        <el-input
+          clearable
+          v-model="query.cinemaName"
+          style="width:200px;"
+          placeholder="请输入影院名称或编码模糊查询"
+        ></el-input>
+        <!-- <cinema-id-selector v-model="query.cinemaId"  style="width:200px;"></cinema-id-selector> -->
+      </el-form-item>
+      <el-form-item label="商家名称">
+        <channel-id-selector v-model="query.channelId" style="width:200px;"></channel-id-selector>
+      </el-form-item>
+      <el-form-item label="省/市">
+        <city-cascader
+          ref="city"
+          v-model="query.area"
+          :rang="1"
+          :clearable="true"
+          placeholder="请选择"
+          style="width:170px;"
+        ></city-cascader>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" @click="refreshTable">查询</el-button>
+        <el-button
+          type="warning"
+          icon="el-icon-refresh"
+          @click="updatePlan(query.channelId,query.cinemaId,query.cinemaName)"
+        >更新排期及价格</el-button>
+      </el-form-item>
+    </el-form>
+    <page-table ref="table" index :query="query" :fetch="queryTable">
+      <el-table-column
+        min-width="180"
+        label="影院名称"
+        align="center"
+        prop="cinemaName"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column min-width="180" label="影院定价分组" align="center" prop="priceRightName"></el-table-column>
+      <el-table-column
+        min-width="140"
+        label="商家名称"
+        align="center"
+        prop="channelName"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column width="80" label="总排期数" align="center" prop="totoalNum"></el-table-column>
+      <el-table-column width="100" label="可售排期数" align="center" prop="sellNum"></el-table-column>
+      <el-table-column min-width="100" label="不可售排期数" align="center" prop="unSellNum"></el-table-column>
+      <el-table-column
+        width="140"
+        label="更新排期时间"
+        align="center"
+        prop="updateTime"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column min-width="200" label="操作" align="center" prop="oper" fixed="right">
+        <template slot-scope="{row}">
+          <el-button type="text" @click="goToManage(row)">管理排期</el-button>
+          <el-button type="text" @click="updatePlan(row.channelId,row.cinemaId,row.cinemaName)">更新排期</el-button>
+        </template>
+      </el-table-column>
+    </page-table>
+  </div>
 </template>
 
 <script>
@@ -75,31 +109,41 @@ export default {
         priceRight: row.priceRight
       }
       sessionStorage.setItem('manage', JSON.stringify(manage))
-      this.$router.push({ path: '/price_center/price_mgr/channel_plan/channel_management' })
+      this.$router.push({
+        path: '/price_center/price_mgr/channel_plan/channel_management'
+      })
     },
     async updatePlan(channelId, cinemaId, cinemaName) {
       if (!cinemaId) {
-        this.$confirm('在未选择渠道商或影院的情况下默认更新所有渠道商的影院排期，此时更新时间会比较长，建议选择单家渠道商进行更新！', '温馨提示', {
-          confirmButtonText: '继续更新',
-          cancelButtonText: '选择单家',
-          type: 'warning'
-        }).then(async() => {
-          this.updataParams = { channelId, cinemaName }
-          const loading = this.$loading({
-            lock: true,
-            text: '正在更新排期',
-            spinner: 'el-icon-loading'
-          })
-          try {
-            await syncChannelPlan(this.updataParams)
-            this.$message.success('系统后台已进入商家影院排期同步中，请稍候返回查看数据')
-            loading.close()
-          } catch (error) {
-            loading.close()
+        this.$confirm(
+          '在未选择渠道商或影院的情况下默认更新所有渠道商的影院排期，此时更新时间会比较长，建议选择单家渠道商进行更新！',
+          '温馨提示',
+          {
+            confirmButtonText: '继续更新',
+            cancelButtonText: '选择单家',
+            type: 'warning'
           }
-        }).catch(() => {
-          this.$message.warning('请选择渠道商')
-        })
+        )
+          .then(async() => {
+            this.updataParams = { channelId, cinemaName }
+            const loading = this.$loading({
+              lock: true,
+              text: '正在更新排期',
+              spinner: 'el-icon-loading'
+            })
+            try {
+              await syncChannelPlan(this.updataParams)
+              this.$message.success(
+                '系统后台已进入商家影院排期同步中，请稍候返回查看数据'
+              )
+              loading.close()
+            } catch (error) {
+              loading.close()
+            }
+          })
+          .catch(() => {
+            this.$message.warning('请选择渠道商')
+          })
       } else {
         this.updataParams = { channelId, cinemaId }
         const loading = this.$loading({
@@ -109,7 +153,9 @@ export default {
         })
         try {
           await syncChannelPlan(this.updataParams)
-          this.$message.success('系统后台已进入商家影院排期同步中，请稍候返回查看数据')
+          this.$message.success(
+            '系统后台已进入商家影院排期同步中，请稍候返回查看数据'
+          )
           loading.close()
         } catch (error) {
           loading.close()
@@ -121,5 +167,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
